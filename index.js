d141b76daccfc206764528694a9ac801254cc9eb
mrebrahim <36786597+mrebrahim@users.noreply.github.com>
2025-05-27 21:35:12 +0300

    index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ try {
 
 // ✅ إعداد Supabase
 const supabaseUrl = "https://lifwzerfuobdppwaowcv.supabase.co";
-const supabaseKey = "eyJhbGciOiJI..."; // حط المفتاح الكامل اللي عندك
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpZnd6ZXJmdW9iZHBwd2Fvd2N2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMTA0NjAsImV4cCI6MjA2MzU4NjQ2MH0.h6hWAkBHdIBV2LITUDWvjGccgIcrpRzuqOv6b1HX8mk"; // حط المفتاح الكامل اللي عندك
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 (async () => {
